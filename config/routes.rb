bcbd122eb8dcd5996858fdf88c72029ad6c1a1ac
corogit <n.ohta5192@gmail.com>
2021-01-07 13:30:55 +0000

    [ルーティング設定]

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,14 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
+  
+  resources :users
+  resources :messages, only: [:create]
+  resources :rooms, only: [:create,:show]
+  resources :genre
+  
+  resources :pets do
+    resource :favorites, only: [:create, :destroy]
+    resources :comments, only: [:create, :destroy]
+  end
 end
