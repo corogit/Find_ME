010885a0816d115787d6fe3045557b9e257a820c
corogit <n.ohta5192@gmail.com>
2021-01-10 06:24:11 +0000

    [マイページ追加]

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,2 +1,51 @@
 class PetsController < ApplicationController
+  
+  def new
+    @pet = Pet.new
+    @genres = Genre.all
+  end
+
+  def create
+    @pet = Pet.new(pet_params)
+    @pet.user_id = current_user.id
+    if @pet.save
+      redirect_to pets_path
+    else
+      @genres = Genre.all
+      render :new
+    end
+  end
+
+  def index
+    @pets = Pet.all
+  end
+
+  def show
+    @pet = Pet.find(params[:id])
+  end
+  
+  def edit
+    @pet = Pet.find(params[:id])
+    @genres = Genre.all
+  end
+  
+  def update
+    @pet = Pet.find(params[:id])
+    if @pet.update(pet_params)
+      redirect_to pet_path
+    else
+      @genres = Genre.all
+      render :edit
+    end
+  end
+
+  def destroy
+  end
+  
+  private
+
+  def pet_params
+    params.require(:pet).permit(:name, :image, :birthday, :gender, :introduction, :genre_id, :user_id)
+  end
+
 end
