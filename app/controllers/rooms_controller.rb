1f6153aa3d6fd0d8a874a453cd646bad86e5cbe7
corogit <n.ohta5192@gmail.com>
2021-01-21 06:57:04 +0000

    [DMレイアウト]

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,5 +1,10 @@
 class RoomsController < ApplicationController
   before_action :authenticate_user!
+  def index
+     @rooms = current_user.rooms.includes(:messages)
+
+  end
+
   def show
     @room = Room.find(params[:id])
     # present?の戻り値は真偽値。よって、trueの場合、
@@ -14,7 +19,7 @@ class RoomsController < ApplicationController
   def create
     @room = Room.create(:name => "DM")
     @entry1 = Entry.create(:room_id => @room.id, :user_id => current_user.id)
-    @entry2 = Entry.create(params.require(:entry).permit(:user_id, :room_id).merge(
+    @entry2 = Entry.create(params.require(:entry).permit(:user_id, :room_id, :full_name).merge(
       :room_id => @room.id
     ))
     redirect_to room_path(@room.id)
