1485f1ed5ecdb0027ce0976222a907382dd38dd3
corogit <n.ohta5192@gmail.com>
2021-01-23 05:24:47 +0000

    [topに戻るボタン追加]

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,11 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+(function() {
+  ('#back a').on('click',function(event){
+    ('body, html').animate({
+      scrollTop:0
+    }, 800);
+    event.preventDefault();
+  });
+});
