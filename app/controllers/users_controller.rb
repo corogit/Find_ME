c11593ac45986603722f4b97ec7a8af4cd2e8e69
corogit <n.ohta5192@gmail.com>
2021-01-25 15:47:02 +0000

    [エラーメッセージ日本語化]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,6 @@
 class UsersController < ApplicationController
   before_action :ensure_correct_user, only: [:edit, :update]
 
-  def index
-    @users = User.all
-  end
 
   def show
     @user = User.find(params[:id])
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -2,5 +2,5 @@ class Genre < ApplicationRecord
   has_many :pets, dependent: :destroy
   
   validates :name, presence: true
-  validates :is_active, presence: true
+  validates :is_active, inclusion: { in: [true, false] }
 end
